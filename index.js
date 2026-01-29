f5ba28773889adf9405d51b6bfab153ba2b7b65e
Donalene Tocmo <donalenetocmo@gmail.com>
2026-01-30 00:22:36 +0800

    update

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import recommended from './configs/recommended.js';
+
 import noConsole from './rules/no-console-left.js';
 import noIncludes from './rules/no-includes-query.js';
 import requireTryCatch from './rules/require-try-catch-api.js';
@@ -10,6 +12,7 @@ export default {
     'require-try-catch-api': requireTryCatch,
     'no-paginate-false': noPaginate
   },
+
   configs: {
     recommended
   }
